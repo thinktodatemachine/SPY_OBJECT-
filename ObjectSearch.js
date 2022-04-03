6737cf46e73c42aca151784a97321cc93654724a
thinktodatemachine <sk1907.com@gmail.com>
2022-04-03 15:25:01 +0530

    ini

--- ObjectSearch.js
+++ ObjectSearch.js
@@ -1,16 +1,19 @@
-// all code goes 
+// code 
+//  creating object
 
 let obj ;
 
 const data = [
     {id:1,name:'some',age:"any"}
 ]
-
+// new entry 
 let newobj = {id:2,name:"aqw",age:"1233"};
 
 data.push(newobj)
 
+// finding specific entry index
 const toModifyIdx = data.findIndex(item => item.id ===2);
+// modifyihg on same index 
 data[toModifyIdx].age=18;
 
 // delete object 
