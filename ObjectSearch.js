ea7fcd48fb420afc4588f54ce0e5beb49de04f30
thinktodatemachine <sk1907.com@gmail.com>
2022-04-03 15:05:04 +0530

    ini

--- ObjectSearch.js
+++ ObjectSearch.js
@@ -6,5 +6,9 @@ const data = [
     {id:1,name:'some',age:"any"}
 ]
 
-let newobj = {id:2,name:"aqw",age:"1233"}
+let newobj = {id:2,name:"aqw",age:"1233"};
+
 data.push(newobj)
+
+const toModifyIdx = data.findIndex(item => item.id ===2);
+data[toModifyIdx].age=18;
